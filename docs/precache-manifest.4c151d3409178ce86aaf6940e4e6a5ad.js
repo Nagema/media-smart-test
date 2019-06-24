47467cc84d4ed60fdceaea53dd5c361ef4d33115
Angelica <angelica.cavero@gmail.com>
2019-06-24 13:01:30 +0200

    docs added

---
+++ docs/precache-manifest.4c151d3409178ce86aaf6940e4e6a5ad.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b8490ee59f58d720150850addfbf21b1",
+    "url": "./index.html"
+  },
+  {
+    "revision": "afa6df91508c914da3b1",
+    "url": "./static/css/main.43325d10.chunk.css"
+  },
+  {
+    "revision": "1354190efe3e7a139cf3",
+    "url": "./static/js/2.81fa0824.chunk.js"
+  },
+  {
+    "revision": "afa6df91508c914da3b1",
+    "url": "./static/js/main.20475a00.chunk.js"
+  },
+  {
+    "revision": "8c97409f0ee389fe75da",
+    "url": "./static/js/runtime~main.d653cc00.js"
+  },
+  {
+    "revision": "951472cc0297f8d2d1b3ea192f2a8518",
+    "url": "./static/media/dot_PNG4.951472cc.png"
+  }
+]);
